853038696aaa02b244284d0fb1367d5bab308fe7
brunogtes <bruno.salmeida@outlook.com.br>
2022-03-25 16:32:45 -0300

    mobile

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -8,18 +8,18 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    
+
     <title>{{ config('app.name', 'Laravel') }}</title>
 
- <!--     Fonts and icons     -->
- <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <!--     Fonts and icons     -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 
-  <!-- CSS Files -->
-  <link href="../css/bootstrap.min.css" rel="stylesheet" />
-  <link href="../css/now-ui-dashboard.css?v=1.5.0" rel="stylesheet" />
+    <!-- CSS Files -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet" />
+    <link href="../css/now-ui-dashboard.css?v=1.5.0" rel="stylesheet" />
 
- 
+
 
 </head>
 
@@ -36,23 +36,23 @@
             </div>
             <div class="sidebar-wrapper" id="sidebar-wrapper">
                 <ul class="nav side-menu">
-                @canany(['paciente', 'funcionario', 'medico', 'admin']) 
+                    @canany(['paciente', 'funcionario', 'medico', 'admin'])
                     <li class="{{ Request::segment(1) === 'inicio' ? 'active' : null }}">
                         <a href="../inicio">
                             <i class="fas fa-home"></i>
                             <p>Inicio</p>
                         </a>
-                    </li>  
+                    </li>
                     @endcanany
-                    @canany(['paciente', 'admin'])                                   
+                    @canany(['paciente', 'admin'])
                     <li class="{{ Request::segment(1) === 'exames' ? 'active' : null }}">
                         <a href="../exames">
                             <i class="fas fa-solid fa-file-medical"></i>
                             <p>Resultado de Exames</p>
                         </a>
-                    </li>    
-                    @endcanany    
-                    @canany(['paciente', 'funcionario', 'medico', 'admin'])            
+                    </li>
+                    @endcanany
+                    @canany(['paciente', 'funcionario', 'medico', 'admin'])
                     <li class="{{ Request::segment(1) === 'perfil' ? 'active' : null }}">
                         <a href="../perfil">
                             <i class="fas fa-user"></i>
@@ -60,14 +60,14 @@
                         </a>
                     </li>
                     @endcanany
-                    @canany(['funcionario', 'admin'])  
+                    @canany(['funcionario', 'admin'])
                     <li class="{{ Request::segment(1) === 'agendamento' ? 'active' : null }}">
                         <a href="../agendamento">
                             <i class="far fa-calendar-alt"></i>
                             <p>Agendamento</p>
                         </a>
                     </li>
-                    <li>                     
+                    <li>
                         <a href="#pageSubCadastros" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"><i class="fas fa-keyboard"></i>
                             <p>Cadastros</p>
                         </a>
@@ -83,7 +83,7 @@
                             </li>
                             <li class="{{ Request::segment(1) === 'cadastroItensExame' ? 'active' : null }}">
                                 <a href="../cadastroItensExame"><i class="fas fa-caret-right"></i>Itens de Exames</a>
-                            </li>                           
+                            </li>
                             <li class="{{ Request::segment(1) === 'cadastroExamesXPlanos' ? 'active' : null }}">
                                 <a href="../cadastroExamesXPlanos"><i class="fas fa-caret-right"></i>Exames X Planos</a>
                             </li>
@@ -96,23 +96,23 @@
 
                         </ul>
                     </li>
-                    @endcanany 
-                    @canany(['funcionario', 'medico', 'admin'])  
+                    @endcanany
+                    @canany(['funcionario', 'medico', 'admin'])
                     <li class="{{ Request::segment(1) === 'cadastroResulExames' ? 'active' : null }}">
                         <a href="../cadastroResulExames">
                             <i class="fa fa-solid fa-microscope"></i>
                             <p>Gestão de Exames</p>
                         </a>
                     </li>
-                    @endcanany 
-                    @canany(['medico', 'admin'])  
+                    @endcanany
+                    @canany(['medico', 'admin'])
                     <li class="{{ Request::segment(1) === 'painelMedico' ? 'active' : null }}">
                         <a href="../painelMedico">
                             <i class="fa fa-solid fa-laptop-medical"></i>
                             <p>Painel Médico</p>
                         </a>
                     </li>
-                    @endcanany 
+                    @endcanany
                     @can('admin')
                     <li>
                         <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"><i class="fas fa-cog"></i>
@@ -130,21 +130,33 @@
                             </li>
                         </ul>
                     </li>
-                    @endcan 
+                    @endcan
 
                 </ul>
             </div>
         </div>
-        
+
         <div class="main-panel" id="main-panel">
             <!-- Navbar -->
             <nav class="navbar navbar-expand-lg navbar-transparent  bg-primary  navbar-absolute">
                 <div class="container-fluid">
                     <div class="navbar-wrapper">
+                        <div class="navbar-toggle">
+                            <button type="button" class="navbar-toggler">
+                                <span class="navbar-toggler-bar bar1"></span>
+                                <span class="navbar-toggler-bar bar2"></span>
+                                <span class="navbar-toggler-bar bar3"></span>
+                            </button>
+                        </div>
 
                         @yield('titulo')
 
                     </div>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-bar navbar-kebab"></span>
+                        <span class="navbar-toggler-bar navbar-kebab"></span>
+                        <span class="navbar-toggler-bar navbar-kebab"></span>
+                    </button>
 
                     <div class="collapse navbar-collapse justify-content-end" id="navigation">
 
@@ -153,20 +165,26 @@
 
                             <li class="nav-item">
                                 <a class="nav-link" href="perfil"><i title="Perfil" class="fas fa-user fa-2x"></i>
+                                    <p>
+                                        <span class="d-lg-none d-md-block"> Perfil</span>
+                                    </p>
 
                                 </a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('logout') }}"  onclick="event.preventDefault();
+                                <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();"><i title="Sair" class="fas fa-sign-out-alt fa-2x"></i>
-                                
+                                    <p>
+                                        <span class="d-lg-none d-md-block"> Sair</span>
+                                    </p>
+
 
                                 </a>
                             </li>
 
                             <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
+                                @csrf
+                            </form>
                         </ul>
                     </div>
                 </div>
@@ -191,18 +209,18 @@
             </footer>
         </div>
     </div>
-  <!--   Core JS Files   -->
-  <script src="../js/core/jquery.min.js"></script>
-  <script src="../js/core/popper.min.js"></script>
-  <script src="../js/core/bootstrap.min.js"></script>
-  <script src="../js/plugins/perfect-scrollbar.jquery.min.js"></script>
+    <!--   Core JS Files   -->
+    <script src="../js/core/jquery.min.js"></script>
+    <script src="../js/core/popper.min.js"></script>
+    <script src="../js/core/bootstrap.min.js"></script>
+    <script src="../js/plugins/perfect-scrollbar.jquery.min.js"></script>
     <!-- Chart JS -->
-  <script src="../js/plugins/chartjs.min.js"></script>
-  <!--  Notifications Plugin    -->
-  <script src="../js/plugins/bootstrap-notify.js"></script>
-  <!-- Control Center for Now Ui Dashboard: parallax effects, scripts for the example pages etc -->
-  <script src="../js/now-ui-dashboard.min.js?v=1.5.0" type="text/javascript"></script><!-- Now Ui Dashboard DEMO methods, don't include it in your project! -->
-  
+    <script src="../js/plugins/chartjs.min.js"></script>
+    <!--  Notifications Plugin    -->
+    <script src="../js/plugins/bootstrap-notify.js"></script>
+    <!-- Control Center for Now Ui Dashboard: parallax effects, scripts for the example pages etc -->
+    <script src="../js/now-ui-dashboard.min.js?v=1.5.0" type="text/javascript"></script><!-- Now Ui Dashboard DEMO methods, don't include it in your project! -->
+
 
 </body>
 
