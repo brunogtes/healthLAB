ca23ded9ae78fc5f3a11b2256aadb068a9c07065
brunogtes <bruno.salmeida@outlook.com.br>
2022-03-25 17:12:16 -0300

    jquery version

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -210,7 +210,7 @@
         </div>
     </div>
     <!--   Core JS Files   -->
-    <script src="../js/core/jquery.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.js"  integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
     <script src="../js/core/popper.min.js"></script>
     <script src="../js/core/bootstrap.min.js"></script>
     <script src="../js/plugins/perfect-scrollbar.jquery.min.js"></script>
@@ -218,8 +218,11 @@
     <script src="../js/plugins/chartjs.min.js"></script>
     <!--  Notifications Plugin    -->
     <script src="../js/plugins/bootstrap-notify.js"></script>
-    <!-- Control Center for Now Ui Dashboard: parallax effects, scripts for the example pages etc -->
-    <script src="../js/now-ui-dashboard.min.js?v=1.5.0" type="text/javascript"></script><!-- Now Ui Dashboard DEMO methods, don't include it in your project! -->
+    <!-- Control Center: parallax effects -->
+    <script src="../js/now-ui-dashboard.min.js?v=1.5.0" type="text/javascript"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.10/jquery.mask.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.2/jquery-confirm.min.js"></script>
 
 
 </body>
